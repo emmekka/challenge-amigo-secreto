195800b39f25ae14a62b552330cddf7f64a3cec9
Melissa <lissak230@gmail.com>
2025-03-17 20:27:53 -0300

    crear funcion sortear amigo

--- app.js
+++ app.js
@@ -3,19 +3,30 @@
 let amigos = [];
 
 function agregarAmigo() {
+    let listaAmigos = document.getElementById('listaAmigos');
     let campo = document.getElementById('amigo');
-    let nombre = campo.value;
+    let nombre = campo.value.trim();
     if (nombre == "") {
         alert("Por favor, inserte un nombre.")
     } else {
         amigos.push(nombre);
-        actualizarLista(nombre);
+        actualizarLista(nombre, listaAmigos);
         campo.value = "";
     }
 }
 
-function actualizarLista(nombre) {
-    let lista = document.getElementById('listaAmigos');
+function actualizarLista(nombre, lista) {
     lista.innerHTML += `<li>${nombre}</li>`;
 }
 
+function sortearAmigo() {
+    let listaResultado = document.getElementById('resultado');
+    let cantAmigos = amigos.length;
+    if (cantAmigos == 0) {
+        alert("Por favor, añada un amigo.");
+    } else {
+        let index = Math.floor(Math.random() * cantAmigos);
+        let amigoSorteado = amigos[index];
+        actualizarLista(amigoSorteado, listaResultado);
+    }
+}
